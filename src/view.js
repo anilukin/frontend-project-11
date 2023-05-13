2820a5d6a2e9aae537edfa2a0d239d213b6f297e
Анастасия Левчук <nastya@MacBook-Air-Anastasia.local>
2023-05-13 20:44:37 +0200

    3.1 step

--- src/view.js
+++ src/view.js
@@ -3,13 +3,13 @@ const renderForm = (st, i18nInstance) => {
   const input = formEl.querySelector('input');
   const feedback = document.querySelector('.feedback');
   if (st.state === 'loadedUrl') {
-    input.classList.remove('border-danger');
+    input.classList.remove('is-invalid');
     feedback.textContent = i18nInstance.t('loadedUrl');
     feedback.classList.remove('text-danger');
     feedback.classList.add('text-success');
   }
   if (st.state === 'errorUrl') {
-    input.classList.add('border-danger');
+    input.classList.add('is-invalid');
     feedback.classList.remove('text-success');
     feedback.classList.add('text-danger');
 
